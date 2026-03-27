343374b9651089fa21c0de5d3a2586eb16ef4058
Thomas <tlindeker0@gmail.com>
2026-03-27 19:54:33 +0100

    v0.5.4: bump DLL version string to 0.5.4

--- kenshi_multiplayer/network.cpp
+++ kenshi_multiplayer/network.cpp
@@ -121,7 +121,7 @@ namespace network {
         hello["t"] = "hello";
         hello["steamName"] = steamName;
         hello["steamId"] = steamId;
-        hello["v"] = "0.4.6";
+        hello["v"] = "0.5.4";
         sendLine(client_fd, hello.dump());
         std::cout << utils::ts() << "Handshake sent." << std::endl;
 
